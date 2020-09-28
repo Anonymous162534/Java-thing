10273b8b024c6a60073bbafa09d6564f4d15a4cf
Nathan Duong <nathanduong2004@yahoo.com>
2020-09-27 22:23:10 -0700

    First batch of Java projects

---
+++ area.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        int length = 20;
+        int width = 5;
+        System.out.print("Perimeter = ");
+        System.out.println(2 * length + 2 * width);
+        System.out.print("Area = ");
+        System.out.println(length * width);
+    }
+}
---
+++ fahrenheit.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        double Celsius = 5;
+        System.out.print("Fahrenheit = ");
+        System.out.println((9.0 / 5.0) * Celsius + 32);
+    }
+}
